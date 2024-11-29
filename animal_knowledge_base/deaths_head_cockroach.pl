3b1df385b1e3ffe32723a29aa8e42821fa4f4a53
Chris Grime <chris@chrisgrime.com>
2024-11-29 15:10:20 -0800

    fix single quote in file

---
+++ animal_knowledge_base/deaths_head_cockroach.pl
@@ -0,0 +1,70 @@
+% Facts about Deaths Head Cockroach
+
+name('Deaths Head Cockroach').
+
+% Physical Characteristics
+animal_scales('Deaths Head Cockroach', false).
+animal_fur('Deaths Head Cockroach', false).
+animal_feathers('Deaths Head Cockroach', false).
+animal_legs('Deaths Head Cockroach', 6).
+animal_teeth('Deaths Head Cockroach', true).
+animal_wings('Deaths Head Cockroach', true).
+animal_weight_lbs('Deaths Head Cockroach', 0.01).
+animal_height_inches('Deaths Head Cockroach', 2).
+animal_has_color('Deaths Head Cockroach', 'Brown').
+animal_has_color('Deaths Head Cockroach', 'Black').
+animal_pattern_type('Deaths Head Cockroach', 'Skull Pattern').
+
+% Behavior
+animal_nocturnal('Deaths Head Cockroach', true).
+animal_hibernates('Deaths Head Cockroach', false).
+animal_lives_in_groups('Deaths Head Cockroach', true).
+animal_social_bonding_level('Deaths Head Cockroach', 'Colonial').
+animal_hoarding_behavior('Deaths Head Cockroach', false).
+
+% Habitat and Environment
+animal_habitat('Deaths Head Cockroach', 'Tropical').
+animal_preferred_environment_temperature('Deaths Head Cockroach', 80).
+animal_territory_size_sq_miles('Deaths Head Cockroach', 0).
+
+% Abilities
+animal_flies('Deaths Head Cockroach', true).
+animal_changes_color('Deaths Head Cockroach', false).
+animal_tree_climbing('Deaths Head Cockroach', 'Poor').
+animal_fishing_ability('Deaths Head Cockroach', 'Poor').
+animal_speech_capability('Deaths Head Cockroach', 'None').
+
+% Reproduction
+animal_eggs_per_clutch('Deaths Head Cockroach', 40).
+animal_clutches_per_year('Deaths Head Cockroach', 4.5).
+
+% Diet
+animal_diet('Deaths Head Cockroach', 'Omnivore').
+animal_diet_preference('Deaths Head Cockroach', 'Plant Matter').
+animal_diet_preference('Deaths Head Cockroach', 'Detritus').
+
+% Other Characteristics
+animal_category('Deaths Head Cockroach', 'Insect').
+animal_lays_eggs('Deaths Head Cockroach', true).
+animal_speed_mph('Deaths Head Cockroach', 3).
+animal_has_a_pouch('Deaths Head Cockroach', false).
+animal_domesticated('Deaths Head Cockroach', true).
+animal_poisonous_or_venomous('Deaths Head Cockroach', false).
+animal_breathes_under_water('Deaths Head Cockroach', false).
+animal_metamorphosis('Deaths Head Cockroach', true).
+animal_average_lifespan('Deaths Head Cockroach', 2).
+animal_temperature('Deaths Head Cockroach', 'Warm').
+animal_common_pet('Deaths Head Cockroach', true).
+animal_endangered('Deaths Head Cockroach', false).
+animal_has_tail('Deaths Head Cockroach', true).
+animal_has_dorsal_fin('Deaths Head Cockroach', false).
+animal_human_interaction('Deaths Head Cockroach', 'Dependent').
+animal_intelligence_level('Deaths Head Cockroach', 'Simple').
+animal_cheek_pouches('Deaths Head Cockroach', false).
+animal_human_usage('Deaths Head Cockroach', 'Pets').
+animal_human_usage('Deaths Head Cockroach', 'Education').
+animal_group_size('Deaths Head Cockroach', 50).
+animal_nest_location('Deaths Head Cockroach', 'Ground Litter').
+animal_migration('Deaths Head Cockroach', false).
+animal_hunting_technique('Deaths Head Cockroach', 'Scavenging').
+animal_burrowing_behavior('Deaths Head Cockroach', 'Tunneling').
