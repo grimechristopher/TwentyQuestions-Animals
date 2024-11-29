32fe011f9eb7c565c257fbdfd10a29d91fcefc10
Chris Grime <chris@chrisgrime.com>
2024-11-29 15:09:22 -0800

    adjust to ask twenty quesitons

--- twenty_questions.py
+++ twenty_questions.py
@@ -10,6 +10,7 @@ class TwentyQuestions:
         self.initialize_database()
         self.animal_probabilities = self.get_all_animals()
         self.questions_asked = 0
+        self.max_questions = 19  # Changed to 19 questions before final guess
 
         self.available_questions = available_questions.copy()
         self.questions_dict = questions
@@ -21,116 +22,32 @@ class TwentyQuestions:
     def get_all_animals(self) -> List[str]:
         # Get all animal names in the database
         results = self.prolog.query("name(X)")
-        # return a key value of name and probability (initialize to 0, sinnce I'm doing count)
+        # return a key value of name and probability (initialize to 0, since I'm doing count)
         return {result["X"]: 0 for result in results}
-    
-    # # A list of possible to ask questions
-    # available_questions = [
-        
-    # ]
-
-    # # I want a dictionary of questions and prolog queries
-    # questions_dict = {
-    #     # Alpaca example using chimp questions
-    #     "Does it have teeth?": {
-    #         "yes": "name(X), animal_teeth(X, true)",
-    #         "no": "name(X), animal_teeth(X, false)",
-    #     },
-    #     "Does it have scales?": {
-    #         "yes": "name(X), animal_scales(X, true)",
-    #         "no": "name(X), animal_scales(X, false)",
-    #     },
-    #     "Is it nocturnal?": {
-    #         "yes": "name(X), animal_nocturnal(X, true)",
-    #         "no": "name(X), animal_nocturnal(X, false)",
-    #     },
-    #     "Does it burrow?": {
-    #         "yes": "name(X), animal_burrowing_behavior(X, Burrowing)",
-    #         "no": "name(X), animal_burrowing_behavior(X, Non-Burrowing)",
-    #     },
-    #     "Can it use tools?": {
-    #         "yes": "name(X), animal_hunting_technique(X, 'Tool Use')",
-    #         "no": "name(X), \+animal_hunting_technique(X, 'Tool Use')",
-    #     },
-    #     "Can it fly?": {
-    #         "yes": "name(X), animal_flies(X, true)",
-    #         "no": "name(X), animal_flies(X, false)",
-    #     },
-    #     "Does it eat meat?": {
-    #         "yes": "name(X), (animal_diet(X, Carnivore); animal_diet(X, Omnivore))",
-    #         "no": "name(X), animal_diet(X, Herbivore)",
-    #     },
-    #     "Is it known for fishing?": {
-    #         "yes": "name(X), animal_fishing_ability(X, Good)",
-    #         "no": "name(X), animal_fishing_ability(X, Poor)",
-    #     },
-    #     "Does it have fur?": {
-    #         "yes": "name(X), animal_fur(X, Fur)",
-    #         "no": "name(X), animal_fur(X, No Fur)",
-    #         "actions": {
-    #             "yes": {
-    #                 "fur": False
-    #             }
-    #         }
-    #     },
-    #     "Is it multi-colored?": {
-    #         "yes": "name(X), animal_pattern_type(X, Multi-Colored)",
-    #         "no": "name(X), animal_pattern_type(X, Solid)",
-    #     },
-    #     "Does it have a tail?": {
-    #         "yes": "name(X), animal_has_tail(X, true)",
-    #         "no": "name(X), animal_has_tail(X, false)",
-    #     },
-    #     "Does it have wings?": {
-    #         "yes": "name(X), animal_wings(X, true)",
-    #         "no": "name(X), animal_wings(X, false)",
-    #     },
-    #     "Is it posinous?": {
-    #         "yes": "name(X), animal_poisonous_or_venomous(X, true)",
-    #         "no": "name(X), animal_poisonous_or_venomous(X, false)",
-    #     },
-    #     "Does this animal sleep in trees?": {
-    #         "yes": "name(X), animal_nest_location(X, 'Tree Branch')",
-    #         "no": "name(X), animal_nest_location(X, 'Ground')",
-    #     },
-    #     "Does it have a pouch?": {
-    #         "yes": "name(X), animal_has_a_pouch(X, true)",
-    #         "no": "name(X), animal_has_a_pouch(X, false)",
-    #     },
-    #     "Does it lay eggs?": {
-    #         "yes": "name(X), animal_lays_eggs(X, true)",
-    #         "no": "name(X), animal_lays_eggs(X, false)",
-    #     },
-    #     "Does it usually live longer than a year?": {
-    #         "yes": "name(X), animal_lifespan(X, Lifespan), Lifespan > 1",
-    #         "no": "name(X), animal_lifespan(X, Lifespan), Lifespan =< 1",
-    #     },
-    #     "Does it live in groups?": {
-    #         "yes": "name(X), animal_lives_in_groups(X, true)",
-    #         "no": "name(X), animal_lives_in_groups(X, false)",
-    #     },
-    #     "Does it eat insects?": {
-    #         "yes": "name(X), animal_diet_preference(X, Insects)",
-    #         "no": "name(X), \+animal_diet_preference(X, Insects)",
-    #     },
-    #     }
 
     def ask_random_question(self):
-        # Select a random question from the dictionary
-        # question = random.choice(list(self.questions_dict.keys()))
-        # first question
-        # question = list(self.questions_dict.keys())[0]
-        question = random.choice(self.questions_dict)
+        # If questions_dict is empty but we haven't reached max questions,
+        # add a random question from available_questions
+        if not self.questions_dict and self.questions_asked < self.max_questions and self.available_questions:
+            random_question = random.choice(self.available_questions)
+            self.questions_dict.append(random_question)
+            self.available_questions.remove(random_question)
 
-
-        print(question.text)
-        return question
+        if self.questions_dict:
+            question = random.choice(self.questions_dict)
+            print(question.text)
+            return question
+        return None
     
     def answer_question(self, question, answer):
+        if not question:
+            return []
+
         query = question.yes_query if answer else question.no_query
         results = self.prolog.query(query)
         animals = [result["X"] for result in results]
-        print(animals)
+        print(answer)
+
         # delete the question from the dictionary
         if question in self.questions_dict:
             self.questions_dict.remove(question)
@@ -148,12 +65,11 @@ class TwentyQuestions:
                         if attribute[answer] in available_question.dependencies:
                             self.questions_dict.append(available_question)
                             added_questions.append(available_question)
-                            # self.available_questions.remove(available_question)
                     for added_question in added_questions:
                         self.available_questions.remove(added_question)
+        
         self.questions_asked += 1
-        # del self.questions_dict[question]
-        return [result["X"] for result in results]
+        return animals
 
 
 def main():
@@ -163,31 +79,30 @@ def main():
     print("\nThink of an animal and press 'Enter' to continue...")
     input()
 
-    # for the length of the questions dictionary, ask a random question
-    # and update the animal probabilities
-    # make an array of the answers for alpaca, yes or no for each question
-    answers = ["no", "no", "no", "no", "no", "no", "no", "no", "yes", "no", "yes", "no", "no", "no", "no", "no", "yes", "yes", "no"]
+    # Array for 19 questions
+    answers = ["no", "no", "no", "no", "no", "no", "no", "no", "yes", "no", 
+              "yes", "no", "no", "no", "no", "no", "yes", "yes", "no"]
+    
     i = 0
-    while twenty_q.questions_dict:
+    while twenty_q.questions_asked < twenty_q.max_questions:
+        print(f"\nQuestion {twenty_q.questions_asked + 1}:")
         question = twenty_q.ask_random_question()
+        
+        if not question:
+            print("No more questions available!")
+            break
 
         answer = True if answers[i].lower() == "yes" else False    
         i += 1    
         animals = twenty_q.answer_question(question, answer)
-        # twenty_q.update_animal_probabilities(animals)
 
-        print()
-        sorted_animals = sorted(twenty_q.animal_probabilities.items(), key=lambda x: x[1], reverse=True)
-        print(sorted_animals)
-        print()
-
-    # Get best matching animal
+    # Get best matching animal - this is question 20
     sorted_animals = sorted(twenty_q.animal_probabilities.items(), key=lambda x: x[1], reverse=True)
+    print(f"\nQuestion 20:")
     print("Is the animal you're thinking of... a " + sorted_animals[0][0] + '?')
 
     print("\nFinal Results:")
     print(sorted_animals)
-    print(twenty_q.questions_dict)
 
 
 if __name__ == "__main__":
